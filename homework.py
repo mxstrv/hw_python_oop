2e37a82fc44ab23f51acd1f00a82f5f9556e1ff0
mxstrv <37843972+mxstrv@users.noreply.github.com>
2022-11-05 16:51:15 +0300

    Use dataclass

--- homework.py
+++ homework.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 class MissingTrainingException(Exception):
     def __init__(self, message, errors):
         super().__init__(message)
@@ -5,15 +8,14 @@ class MissingTrainingException(Exception):
         print(errors)
 
 
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-
-    def __init__(self, training_type, duration, distance, speed, calories):
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
         return (f'Тип тренировки: {self.training_type};'
